d293136cba698d3d7eb35fdc106a762816e26c9c
Pedro Assis <pedjor@gmail.com>
2022-01-05 22:37:33 +0000

    Added tests to open instrument

--- temp/Alive.py
+++ temp/Alive.py
@@ -1,4 +1,4 @@
-print ("I'm Alive and sometimes I kick")
+print("I'm Alive and sometimes I kick")
 
 
 """
@@ -16,20 +16,23 @@ import time
 from serial import SerialException
 """
 
-address='TCPIP0::192.168.120.50::inst0::INSTR'
+address = 'TCPIP0::192.168.120.50::inst0::INSTR'
 #
 rm = pyvisa.ResourceManager('@py')  # use py-visa backend
 #rm = pyvisa.ResourceManager()  # use py-visa backend
 #print(rm.list_resources())
-keith = rm.open_resource('TCPIP::192.168.120.50::inst0::INSTR')
-#print ('opening instrument')
-#print (self.inst)
-if (keith): 
+try:
+    keith = rm.open_resource('TCPIP0::192.168.120.50::inst0::INSTR')
+    #print ('opening instrument')
+    #print (self.inst)
+except:
+    print ("UhOh, something went worng")
+    exit(1)
+else:
+#'if (keith):
     print('I could open it and it\'s called:')
-print(keith.query('*IDN?'))
-
-
-keith.close()
+    print(keith.query('*IDN?'))
+    keith.close()
 
 
 
